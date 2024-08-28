38934f427eaabc223127a9e7e70fbfe769a3a38a
Oliver <35813223+Oliver-Jupp@users.noreply.github.com>
2024-08-29 00:57:09 +0100

    Changed to using batches. Changed output to a file per batch.

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import chess.pgn
 
 import multiprocessing
 
+def findAmountOfGames(filePath):
+    gameCounter = 0
+    with open(filePath, mode="r") as file:
+        for line in file:
+            if line.startswith("[Event "):
+                gameCounter += 1
+    return gameCounter
+
 def getAllFen(game):
     board = game.board()
 
@@ -38,6 +46,7 @@ if __name__ == "__main__":
 
     parser.add_argument("-f", "--file", help="Path to the input file", required=True)
     parser.add_argument("-o", "--output", help="Path to the output file / directory", default=".")
+    parser.add_argument("-b", "--batches", help="Amount of batches to run program with", default=50)
 
     args = parser.parse_args()
 
@@ -52,17 +61,50 @@ if __name__ == "__main__":
         if os.path.exists(args.output):
             outFile = os.path.abspath(args.output)
             if os.path.isdir(outFile):
-                outFile = os.path.join(outFile, "output.txt")
+                outFile = os.path.join(outFile, "output")
         else:
             print(f"Error: The output path '{args.output}' does not exist.")
             sys.exit(1)
+        
+    if args.batches:
+        try:
+            amountBatches = int(args.batches)
+        except ValueError:
+            print(f"Error: Amount of batches: '{args.batches}' cannot be parsed as an int.")
+            sys.exit(1)
 
-    games = retrieveAllGames(filePath)
 
-    with multiprocessing.Pool() as pool:
-        setOfAllFen = pool.map(getAllFen, games)
+    
 
-    allFen = set().union(*setOfAllFen)
+    # Find amount of games in file
+    print("Counting total games...")
+    totalGames = findAmountOfGames(filePath)
+    print("Total games:", totalGames)
 
-    saveFenToFile(allFen, outFile)
-    print("Output written to:", outFile)
+    gamesPerBatch = totalGames // amountBatches
+    remainingGames = totalGames % amountBatches
+
+    file = open(filePath, "r")
+
+    for batchIndex in range(amountBatches):
+        print(f"Working on batch {batchIndex + 1}")
+        batchGames = []
+
+        for _ in range(gamesPerBatch + (1 if batchIndex < remainingGames else 0)):
+            game = chess.pgn.read_game(file)
+            if game is None:
+                break
+            batchGames.append(game)
+        
+        print(f"Extracting FEN's")
+
+        with multiprocessing.Pool() as pool:
+            setOfAllFen = pool.map(getAllFen, batchGames)
+
+        allFen = set().union(*setOfAllFen)
+
+        batchOutFile = str(outFile) + "_batch_" + str(batchIndex+1) + ".txt"
+        saveFenToFile(allFen, batchOutFile)
+    
+    print("Completed")
+
